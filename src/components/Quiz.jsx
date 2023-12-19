6c6a601d1b172805ab08cf313df6285c1f46fd1f
Marco Mercado <marco@stonepawtraining.com>
2023-12-19 17:05:11 -0500

    Reorganized components

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,13 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions";
-import QuestionTimer from "./QuestionTimer";
 import quizCompleteImg from "../assets/quiz-complete.png";
+import Question from "./Question";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
@@ -33,26 +32,14 @@ export default function Quiz() {
     );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
-
-  const answers = shuffledAnswers.map((answer) => (
-    <li key={answer} className="answer">
-      <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-    </li>
-  ));
-
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={10000}
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">{answers}</ul>
-      </div>
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
