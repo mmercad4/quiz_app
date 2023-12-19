dc79b64a915d455e0a27e703adaefd32ec2f6c9c
Marco Mercado <marco@stonepawtraining.com>
2023-12-19 15:32:19 -0500

    Added a quiz complete screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -13,7 +16,19 @@ export default function Quiz() {
     });
   }
 
-  const answers = QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  const answers = shuffledAnswers.map((answer) => (
     <li key={answer} className="answer">
       <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
     </li>
