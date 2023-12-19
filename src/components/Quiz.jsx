fb431605c4ac916df157c1f6273c3eb7fe30e1e4
Marco Mercado <marco@stonepawtraining.com>
2023-12-19 10:09:27 -0500

    Added Header and Quiz components

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import QUESTIONS from "../questions";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  const answers = QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+    <li key={answer} className="answer">
+      <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+    </li>
+  ));
+
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">{answers}</ul>
+    </div>
+  );
+}
